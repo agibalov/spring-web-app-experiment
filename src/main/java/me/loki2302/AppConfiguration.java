a50acd7610aa391b14e937f0712b1c67b5db5f14
loki2302 <loki2302@loki2302.me>
2013-09-14 17:42:26 +0400

    Now using Jade as view engine

--- src/main/java/me/loki2302/AppConfiguration.java
+++ src/main/java/me/loki2302/AppConfiguration.java
@@ -2,17 +2,37 @@ package me.loki2302;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import de.neuland.jade4j.JadeConfiguration;
+import de.neuland.jade4j.filter.MarkdownFilter;
+import de.neuland.jade4j.spring.template.SpringTemplateLoader;
+import de.neuland.jade4j.spring.view.JadeViewResolver;
 
 @EnableWebMvc
 @ComponentScan("me.loki2302")
 public class AppConfiguration {
     @Bean
-    public InternalResourceViewResolver provideInternalResourceViewResolver() {
-        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setPrefix("/");
-        internalResourceViewResolver.setSuffix(".jsp");
-        return internalResourceViewResolver;
+    public SpringTemplateLoader templateLoader() {
+        SpringTemplateLoader templateLoader = new SpringTemplateLoader();
+        templateLoader.setBasePath("/");
+        templateLoader.setEncoding("UTF-8");
+        templateLoader.setSuffix(".jade");
+        return templateLoader;
+    }
+
+    @Bean
+    public JadeConfiguration jadeConfiguration(SpringTemplateLoader templateLoader) {
+        JadeConfiguration configuration = new JadeConfiguration();
+        configuration.setCaching(false);
+        configuration.setTemplateLoader(templateLoader);
+        return configuration;
+    }
+
+    @Bean
+    public ViewResolver viewResolver(JadeConfiguration jadeConfiguration) {
+        JadeViewResolver viewResolver = new JadeViewResolver();
+        viewResolver.setConfiguration(jadeConfiguration);
+        return viewResolver;
     }
 }
--- src/main/webapp/index.jsp
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-<p>Current time is ${currentTime}</p>
-<p>Service says ${whatServiceSays}</p>
-</body>
-</html>
