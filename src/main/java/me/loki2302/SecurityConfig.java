c94c4d1689acbdfbade4ec199f90b235fea42b8c
loki2302 <loki2302@loki2302.me>
2013-09-22 18:59:12 +0400

    Minor progress

--- src/main/java/me/loki2302/SecurityConfig.java
+++ src/main/java/me/loki2302/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/assets/**");
+        web.ignoring().antMatchers("/assets/**", "/robots.txt");
     }
     
     @Override
