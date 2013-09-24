3b0e5ed6e3790d1f3638d59acfe60dd45c737775
loki2302 <loki2302@loki2302.me>
2013-09-24 13:56:00 +0400

    Progress with authentication service

--- src/main/java/me/loki2302/service/AuthenticationService.java
+++ src/main/java/me/loki2302/service/AuthenticationService.java
@@ -3,6 +3,8 @@ package me.loki2302.service;
 import me.loki2302.dao.UserDao;
 import me.loki2302.dao.rows.UserRow;
 import me.loki2302.service.exceptions.IncorrectPasswordException;
+import me.loki2302.service.exceptions.UserNameAlreadyUsedException;
+import me.loki2302.service.exceptions.UserNotRegisteredException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,10 +29,26 @@ public class AuthenticationService {
     }
     
     public int signIn(String userName, String password) {
-        throw new RuntimeException();
+        UserRow user = userDao.findUserByUserName(userName);
+        if(user == null) {
+            throw new UserNotRegisteredException();
+        }
+        
+        if(!user.Password.equals(password)) {
+            throw new IncorrectPasswordException();
+        }
+        
+        return user.Id;
     }
     
     public int signUp(String userName, String password) {
-        throw new RuntimeException();
+        UserRow user = userDao.findUserByUserName(userName);
+        if(user != null) {
+            throw new UserNameAlreadyUsedException();
+        }
+        
+        user = userDao.createUser(userName, password);
+        
+        return user.Id;        
     }
 }
---
+++ src/main/java/me/loki2302/service/exceptions/UserNameAlreadyUsedException.java
@@ -0,0 +1,5 @@
+package me.loki2302.service.exceptions;
+
+public class UserNameAlreadyUsedException extends RuntimeException {
+    private static final long serialVersionUID = 1L;        
+}
