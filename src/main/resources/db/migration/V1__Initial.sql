70da85d84362d062c850f7c4361a910dad4abb1a
loki2302 <loki2302@loki2302.me>
2013-10-01 12:06:50 +0400

    Updated DDL

--- src/main/resources/db/migration/V1__Initial.sql
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -2,7 +2,7 @@ create table Something(x int);
 insert into Something(x) values(123);
 
 create table Users(
-	Id int identity,
+	Id int identity primary key,
 	Name varchar(256) not null,
 	Password varchar(256) not null,
 	Type int not null
@@ -11,52 +11,43 @@ create table Users(
 create unique index UserNameIndex on Users(Name);
 
 create table Sessions(
-	Id int identity,
+	Id int identity primary key,
 	Token char(36) not null,
-	UserId int not null	
+	UserId int not null constraint SessionUserRef references Users(Id)	
 );
 
-alter table Sessions add foreign key (UserId) references Users (Id);
+create unique index SessionTokenIndex on Sessions(Token);
 
 create table Categories(
-	Id int identity,
+	Id int identity primary key,
 	Name varchar(256) not null 
 );
 
 create table Articles(
-	Id int identity,
+	Id int identity primary key,
 	Title varchar(256) not null,
 	Text varchar(65536) not null,
 	CreatedAt timestamp not null,
 	UpdatedAt timestamp,
 	ReadCount int not null,
-	UserId int not null,
-	CategoryId int not null
+	UserId int not null constraint ArticleUserRef references Users(Id),
+	CategoryId int not null constraint ArticleCategoryRef references Categories(Id)
 );
 
-alter table Articles add foreign key (UserId) references Users (Id);
-alter table Articles add foreign key (CategoryId) references Categories (Id);
-
 create table Comments(
-	Id int identity,
+	Id int identity primary key,
 	Text varchar(65536) not null,
 	CreatedAt timestamp not null,
 	UpdatedAt timestamp,
-	ArticleId int not null,
-	UserId int not null
+	ArticleId int not null constraint CommentArticleRef references Articles(Id),
+	UserId int not null constraint CommentUserRef references Users(Id)
 );
-
-alter table Comments add foreign key (ArticleId) references Articles (Id);
-alter table Comments add foreign key (UserId) references Users (Id);
 
 create table ArticleVotes(
-	Id int identity,
+	Id int identity primary key,
 	CreatedAt timestamp not null,
 	UpdatedAt timestamp,
-	ArticleId int not null,
-	UserId int not null,
-	Vote int not null
+	Vote int not null,
+	ArticleId int not null constraint ArticleVoteArticleRef references Articles(Id),
+	UserId int not null constraint ArticleVoteUserRef references Users(Id)	
 );
-
-alter table ArticleVotes add foreign key (ArticleId) references Articles (Id);
-alter table ArticleVotes add foreign key (UserId) references Users (Id);
