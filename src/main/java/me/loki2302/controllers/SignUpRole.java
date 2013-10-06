b32b55fed1247d481647471e65d778ff22b6a409
loki2302 <loki2302@loki2302.me>
2013-10-06 23:57:16 +0400

    Minor tweaks

--- src/main/java/me/loki2302/controllers/SignUpRole.java
+++ src/main/java/me/loki2302/controllers/SignUpRole.java
@@ -1,12 +1,6 @@
 package me.loki2302.controllers;
 
 public enum SignUpRole {
-    Reader("Reader"),
-    Writer("Writer");
-    
-    private final String roleName; // how does it really work?
-    
-    SignUpRole(String roleName) {
-        this.roleName = roleName;
-    }
+    Reader,
+    Writer
 }
