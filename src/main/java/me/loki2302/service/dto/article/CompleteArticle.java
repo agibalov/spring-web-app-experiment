97c84cb68f0c12aa81e7a5cdab0c2b6e64c7c261
loki2302 <loki2302@loki2302.me>
2013-10-06 19:49:45 +0400

    Cleaning up

--- src/main/java/me/loki2302/service/dto/article/CompleteArticle.java
+++ src/main/java/me/loki2302/service/dto/article/CompleteArticle.java
@@ -3,17 +3,7 @@ package me.loki2302.service.dto.article;
 import java.util.List;
 
 public class CompleteArticle extends BriefArticle {
-    //public int ArticleId;
-    //public String Title;
     public String Text;
-    //public Date CreatedAt;
-    //public Date UpdatedAt;
-    //public int ReadCount;
-    //public int CommentCount;
-    //public int VoteCount;
-    //public int AverageVote;
-    //public BriefUser User;
-    //public BriefCategory Category;
     public List<Comment> Comments;
     public boolean CanVote;
     public Integer CurrentVote;
--- src/main/java/me/loki2302/service/dto/article/ShortArticle.java
+++ src/main/java/me/loki2302/service/dto/article/ShortArticle.java
@@ -1,15 +1,5 @@
 package me.loki2302.service.dto.article;
 
 public class ShortArticle extends BriefArticle {
-    //public int ArticleId;
-    //public String Title;
     public String FirstParagraph;
-    //public Date CreatedAt;
-    //public Date UpdatedAt;
-    //public int ReadCount;
-    //public int CommentCount;
-    //public int VoteCount;
-    //public int AverageVote;
-    //public BriefUser User;
-    //public BriefCategory Category;
 }
