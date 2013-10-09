46ada1a2166dbcccfe5a0f7a1c7001869c91d436
loki2302 <loki2302@loki2302.me>
2013-10-09 16:18:41 +0400

    Minor refactoring

---
+++ src/test/java/me/loki2302/AbstractServiceTest.java
@@ -0,0 +1,14 @@
+package me.loki2302;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfiguration.class)
+@Transactional
+@TransactionConfiguration(defaultRollback = true)
+public abstract class AbstractServiceTest {
+}
--- src/test/java/me/loki2302/AuthenticationServiceTest.java
+++ src/test/java/me/loki2302/AuthenticationServiceTest.java
@@ -8,19 +8,10 @@ import me.loki2302.service.exceptions.UserNameAlreadyUsedException;
 import me.loki2302.service.exceptions.UserNotRegisteredException;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 import static org.junit.Assert.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TestConfiguration.class)
-@Transactional
-@TransactionConfiguration(defaultRollback = true)
-public class AuthenticationServiceTest {
+public class AuthenticationServiceTest extends AbstractServiceTest {
     @Autowired
     private AuthenticationService authenticationService;
     
