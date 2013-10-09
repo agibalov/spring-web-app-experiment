7c240e0972472a202e856271fa71827afa1149c2
loki2302 <loki2302@loki2302.me>
2013-10-09 12:54:30 +0400

    Minor progress with tests

--- src/test/java/me/loki2302/AuthenticationServiceTest.java
+++ src/test/java/me/loki2302/AuthenticationServiceTest.java
@@ -1,6 +1,10 @@
 package me.loki2302;
 
 import me.loki2302.service.AuthenticationService;
+import me.loki2302.service.UserType;
+import me.loki2302.service.dto.AuthenticationResult;
+import me.loki2302.service.exceptions.IncorrectPasswordException;
+import me.loki2302.service.exceptions.UserNameAlreadyUsedException;
 import me.loki2302.service.exceptions.UserNotRegisteredException;
 
 import org.junit.Test;
@@ -10,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfiguration.class)
@@ -20,7 +25,43 @@ public class AuthenticationServiceTest {
     private AuthenticationService authenticationService;
     
     @Test(expected = UserNotRegisteredException.class)
-    public void canSignInIfDidNotSignUpBefore() {
+    public void cantSignInIfNotRegistered() {
         authenticationService.signIn("loki2302", "qwerty");        
+    }
+    
+    @Test
+    public void canSignInIfRegistered() {
+        AuthenticationResult authResultA = authenticationService.signUp("loki2302", "qwerty", UserType.Reader);
+        assertAdequateAuthenticationResult(authResultA);
+        
+        AuthenticationResult authResultB = authenticationService.signIn("loki2302", "qwerty");
+        assertAdequateAuthenticationResult(authResultB);
+        
+        assertEquals(authResultA.UserId, authResultB.UserId);
+    }
+    
+    @Test
+    public void canSignUp() {
+        AuthenticationResult authenticationResult = 
+                authenticationService.signUp("loki2302", "qwerty", UserType.Reader);
+        assertAdequateAuthenticationResult(authenticationResult);
+    }
+    
+    @Test(expected = UserNameAlreadyUsedException.class)
+    public void cantSignUpTwice() {
+        authenticationService.signUp("loki2302", "qwerty", UserType.Reader);
+        authenticationService.signUp("loki2302", "qwerty", UserType.Reader);
+    }
+    
+    @Test(expected = IncorrectPasswordException.class)
+    public void cantSignInWithIncorrectPassword() {
+        authenticationService.signUp("loki2302", "qwerty", UserType.Reader);
+        authenticationService.signIn("loki2302", "qwert");
+    }
+    
+    private static void assertAdequateAuthenticationResult(AuthenticationResult authenticationResult) {
+        assertNotNull(authenticationResult.SessionToken);        
+        assertNotNull(authenticationResult.UserName);
+        assertNotNull(authenticationResult.UserType);
     }
 }
